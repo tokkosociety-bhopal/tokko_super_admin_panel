0705c9cc575198d3260e7f5057d1c0c237b62989
sarthakpersonal755-6581 <sarthakpersonal755@gmail.com>
2026-05-20 19:02:27 +0530

    fix api key issue

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,18 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBkNHL1BBBQJ9U77q-2CPfI87f8lxOOFc",
-  authDomain: "tokko-society-19052026.firebaseapp.com",
-  projectId: "tokko-society-19052026",
-  storageBucket: "tokko-society-19052026.firebasestorage.app",
-  messagingSenderId: "100599934",
-  appId: "1:100599934:web:1c23c68b42857096db702f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app =
-  getApps().length === 0
-    ? initializeApp(firebaseConfig)
-    : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
